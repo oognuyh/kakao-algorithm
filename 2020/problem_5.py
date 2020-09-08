3d100dd4ac336a9464e3b46fca6662c68f84287f
oognuyh <48203569+oognuyh@users.noreply.github.com>
2020-09-08 22:46:28 +0900

    hg

--- 2020/problem_5.py
+++ 2020/problem_5.py
@@ -83,11 +83,3 @@ def rollback(command, columns, beams):
         beams[y][x] = BEAM if b == DELETE else EMPTY
 
 # build_frame의 가로가 4인데 지어지는 공간의 가로가 4로 착각하여 오래걸림
-
-if __name__ == "__main__":
-    print('- ' * 30)
-    print('\n'.join(map(str, solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))))
-    print('- ' * 30)
-    
-    print('\n'.join(map(str, solution(5, [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))))
-    print('- ' * 30)
